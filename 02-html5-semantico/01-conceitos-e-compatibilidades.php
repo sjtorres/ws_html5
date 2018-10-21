7c2de44872e123aa3f2a82774a45d2221903a375
Sandro Torres <sandrotorres.ti@gmail.com>
2018-10-21 08:58:53 -0200

    Iniciando marcação semântica

---
+++ 02-html5-semantico/01-conceitos-e-compatibilidades.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        
+        <title>Conceitos e Compatibilidades</title>
+        
+        <!--[if lt IE 9]>
+            <script src="js/html5shiv.js"></script>
+        <![endif]-->
+        
+        
+    </head>
+    <body>
+
+    </body>
+</html>
---
+++ 02-html5-semantico/02-iniciando-marcacao.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php
+define('BASE', 'http://localhost/ws_html5/02-html5-semantico/');
+?>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Curso Work Series - HTML5 do Jeito Certo!</title>
+
+        <!--[if lt IE 9]>
+            <script src="js/html5shiv.js"></script>
+        <![endif]-->
+
+    </head>
+    <body>
+
+        <header class="main_header">
+            <h1 class="main_logo">
+                <a title="Home" href="<?= BASE; ?>">
+                    Curso Work Series - HTML5 do Jeito Certo!
+                </a>
+            </h1>
+            <ul class="main_nav">
+                <li><a title="" href=""></a></li>
+            </ul>
+        </header>
+
+        <!--CONTEÚDO-->
+
+        <footer class="main_footer">
+            <section>
+                <h1>UpInside Treinamentos</h1>
+            </section>
+        </footer>
+    </body>
+</html>
